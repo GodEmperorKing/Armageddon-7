2626d2ff427068a2f2a7426ea17e033df24445f4
GodEmperorKing <youralltoeasy@gmail.com>
2026-04-14 11:54:37 -0700

    Update lab-3a-saopaulo-vpc.tf

--- lab-3a-saopaulo-vpc.tf
+++ lab-3a-saopaulo-vpc.tf
@@ -1,1 +1,29 @@
+### Sao Paulo VPC (The Liberdade Outer Rim)
 
+### Explanation: We must explicitly tell Terraform to build this in Brazil.
+provider "aws" {
+  alias  = "saopaulo"
+  region = "sa-east-1"
+}
+
+### Explanation: The new isolated network for the clinics, using your specific student IP block.
+resource "aws_vpc" "liberdade_vpc01" {
+  provider             = aws.saopaulo
+  cidr_block           = "10.239.0.0/16" 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "liberdade-vpc01"
+  }
+}
+
+### Explanation: An Internet Gateway so the Brazil instances can eventually reach out for patches.
+resource "aws_internet_gateway" "liberdade_igw01" {
+  provider = aws.saopaulo
+  vpc_id   = aws_vpc.liberdade_vpc01.id
+
+  tags = {
+    Name = "liberdade-igw01"
+  }
+}

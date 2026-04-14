17c08ed2394237f4d692df9602b7f7cf33424a1f
GodEmperorKing <youralltoeasy@gmail.com>
2026-04-13 20:41:12 -0700

    Update lab-3a-tgw.tf

--- lab-3a-tgw.tf
+++ lab-3a-tgw.tf
@@ -1,8 +1,6 @@
-############################################
-    # Tokyo Transit Gateway (The Shinjuku Hub)
-    ############################################
+    ### Tokyo Transit Gateway (The Shinjuku Hub)
     
-    # Explanation: This is the central router for the Japan Medical network.
+    ### Explanation/Context: This is the central router for the Japan Medical network.
     resource "aws_ec2_transit_gateway" "shinjuku_tgw01" {
       description = "Tokyo Hub Transit Gateway"
       tags = {
@@ -10,7 +8,7 @@
       }
     }
     
-    # Explanation: Attaching the existing Lab 2 VPC to the new Hub.
+    ### Explanation: Attaching the existing Lab 2 VPC to the new Hub.
     resource "aws_ec2_transit_gateway_vpc_attachment" "shinjuku_vpc_attach01" {
       subnet_ids         = aws_subnet.palpaking_private_subnets[*].id
       transit_gateway_id = aws_ec2_transit_gateway.shinjuku_tgw01.id
